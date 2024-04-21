a0b88e69cb2f5ba5b5db19d80a22f29c012a172d
honnkon <nkNh0.505@gmail.com>
2024-04-21 10:18:33 +0900

    Service Workerを使用したキャッシュ

--- equaline-updates/script.js
+++ equaline-updates/script.js
@@ -1,5 +1,17 @@
 "use strict";
 
+if ('serviceWorker' in navigator) {
+	window.addEventListener('load', () => {
+		navigator.serviceWorker.register('/sw.js')
+		.then(registration => {
+			console.log('ServiceWorker registration successful with scope: ', registration.scope);
+		})
+		.catch(err => {
+			console.log('ServiceWorker registration failed: ', err);
+		});
+	});
+}
+
 function NewUpdateLine(Date, Version, Data) {
 	$("main").append(`<div class="Content">
 		<div class="Update">
---
+++ sw.js
@@ -0,0 +1,63 @@
+const CACHE_NAME = 'date2024-04-21v1';
+const urlsToCache = [
+	'/index.html',
+	'/script.js',
+	'/main-style.css',
+	'/jquery.min.js',
+	'/404.html'
+];
+
+self.addEventListener('install', event => {
+	// インストール処理
+	event.waitUntil(
+		caches.open(CACHE_NAME)
+			.then(cache => {
+				console.log('Opened cache');
+				return cache.addAll(urlsToCache);
+			})
+	);
+});
+
+self.addEventListener('fetch', event => {
+	event.respondWith(
+		caches.match(event.request).then(cachedResponse => {
+			// キャッシュがあればそのレスポンスを返す
+			if (cachedResponse) {
+				return cachedResponse;
+			}
+
+			// ネットワークリクエストを行い、レスポンスを取得
+			return fetch(event.request).then(response => {
+				// 有効なレスポンスか確認（オプション）
+				if (!response || response.status !== 200 || response.type !== 'basic') {
+					return response;
+				}
+
+				// レスポンスをクローンしてキャッシュに保存
+				let responseToCache = response.clone();
+
+				caches.open(CACHE_NAME).then(cache => {
+					cache.put(event.request, responseToCache);
+				});
+
+				return response;
+			});
+		})
+	);
+});
+
+self.addEventListener('activate', event => {
+	event.waitUntil(
+		caches.keys().then(cacheNames => {
+			return Promise.all(
+				cacheNames.filter(cacheName => {
+					// CACHE_NAMEに指定されていないキャッシュを削除
+					return cacheName !== CACHE_NAME;
+				}).map(cacheName => {
+					// 古いキャッシュを削除
+					return caches.delete(cacheName);
+				})
+			);
+		})
+	);
+});

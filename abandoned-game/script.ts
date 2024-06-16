3f30fe761ad7b7bc9270340ba84eccad5b25eed7
honnkon <nkNh0.505@gmail.com>
2024-06-16 11:00:54 +0900

    とりあえず放置ゲームの更新

--- abandoned-game/script.ts
+++ abandoned-game/script.ts
@@ -1,37 +1,41 @@
-"use strict";
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
         navigator.serviceWorker.register('/sw.js')
             .then(registration => {
-            console.log('ServiceWorker registration successful with scope: ', registration.scope);
-        })
+                console.log('ServiceWorker registration successful with scope: ', registration.scope);
+            })
             .catch(err => {
-            console.log('ServiceWorker registration failed: ', err);
-        });
+                console.log('ServiceWorker registration failed: ', err);
+            });
     });
 }
-let Point = 0;
-let BuyCount = [0];
-let SecondPoint = 1;
-$(function () {
+
+let Point: number = 0;
+let BuyCount: number[] = [0];
+let SecondPoint: number = 1;
+$(function (): void {
     $("div#shop button").on("click", Buy)
         .hover(Hover);
     PrevTime = Date.now();
     Update();
-});
-let PrevTime;
-function Buy() {
-    const id = $("div#shop").children().index(this);
+})
+
+let PrevTime: number;
+
+function Buy(this: HTMLElement): void {
+    const id: number = $("div#shop").children().index(this);
     if (Point >= Cost(id)) {
         Point -= Cost(id);
         BuyCount[id] += 1;
     }
 }
-function Hover() {
+
+function Hover(this: HTMLElement): void {
     $("#item").text($(this).text());
     $("#item-cost").text(Cost($("div#shop").children().index(this)));
 }
-function Update() {
+
+function Update(): void {
     SecondPoint = 1 + BuyCount[0];
     Point += SecondPoint * (Date.now() - PrevTime) / 1000;
     $('#point').text(`${Math.floor(Point)}ポイント`);
@@ -39,7 +43,10 @@ function Update() {
     PrevTime = Date.now();
     requestAnimationFrame(Update);
 }
-function Cost(id) {
-    return (id == 0 ? Math.floor((1.4 ** (BuyCount[id] * 1.4)) - (1.1 ** (BuyCount[id] * 0.08))) + 10 :
-        0);
+
+function Cost(id:number): number {
+    return (
+        id==0?Math.floor((1.4 ** (BuyCount[id] * 1.4))-(1.1 ** (BuyCount[id] * 0.08)))+10:
+        0
+    );
 }

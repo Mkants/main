45c9b79a5a593402092c2a299ea642bd6594979f
honnkon <nkNh0.505@gmail.com>
2024-07-31 16:53:57 +0900

    リダイレクト設定の修正

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-if (location.host !== "honnkon.tech" || location.host !== "honnkon314.f5.si") {
-	location.replace("https://honnkon.tech");
-}
-
 if ('serviceWorker' in navigator) {
 	window.addEventListener('load', () => {
 		navigator.serviceWorker.register('/sw.js')
